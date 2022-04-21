29e99b5f98f136537bf66bf4aa71fb78769e67e3
Curtis Covington <curtis.covington@gmail.com>
2022-04-21 10:05:30 -0600

    quality of life improvements

--- ColorThresh2.py
+++ ColorThresh2.py
@@ -3,6 +3,7 @@ from operator import truediv
 import cv2 as cv
 import argparse
 import numpy as np
+import pyautogui
 print(cv.__version__)
 
 
@@ -13,17 +14,18 @@ def isLeft(a, b, c):
 
 class ColorDetector():
     colorMode = True
-    width = 1920
-    height = 1080
     capture = None
     top = []
     bot = []
     left = []
     right = []
 
+    defaultLower =  np.array([11,172,176], np.uint8)
+    defaultUpper =  np.array([52,183,254], np.uint8)
     # some blue
-    colorLower = np.array([110,235,165], np.uint8)
-    colorUpper = np.array([130,255,255], np.uint8)
+    colorLower = defaultLower
+    colorUpper = defaultUpper
+    color = None
 
     threshold = 10
 
@@ -34,8 +36,29 @@ class ColorDetector():
     vRange = 40
     
     lastClick = None
+    testImg = None
+    showNormal = False
+    current_direction = None
     
     def __init__(self):
+        self.hRange = 0
+        self.sRange = 0
+        self.vRange = 0
+
+        # create an artificial image where half the image is colorLower and half is colorUpper
+        # this is to test the color thresholding
+        self.testImg = np.zeros((400, 400, 3), np.uint8)
+        self.testImg[:, :, 0] = np.arange(0, 400, dtype=np.uint8)
+        self.testImg[:, :, 1] = np.arange(0, 400, dtype=np.uint8)
+        self.testImg[:, :, 2] = np.arange(0, 400, dtype=np.uint8)
+
+        self.capture = cv.VideoCapture(0)
+        if not self.capture.isOpened():
+            exit(0)
+
+        self.width = int(self.capture.get(cv.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.capture.get(cv.CAP_PROP_FRAME_HEIGHT))
+
         for x in range(0,self.width):
             for y in range(0,self.height):
                 leftLine = True
@@ -58,13 +81,35 @@ class ColorDetector():
                     # add index to left triangle indices
                     self.top.append((y, x))
     
+    def updateTestImage(self):
+        
+        # convert colorLower pixel and colorUpper pixel from hsv to bgr
+        
+        # colorLower = cv.cvtColor(np.uint8([[self.colorLower]]), cv.COLOR_HSV2BGR)
+        # colorUpper = cv.cvtColor(np.uint8([[self.colorUpper]]), cv.COLOR_HSV2BGR)
+        # everything below y = 100 is the colorLower
+        # everything above y = 100 is the colorUpper
+        self.testImg[:, :] = self.colorUpper
+        self.testImg[(200,200)[0]:, :] = self.colorLower
+
+        # put the hsv value on the image
+        cv.putText(self.testImg, str(self.colorUpper), (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv.putText(self.testImg, str(self.colorLower), (10, 230), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        self.testImg = cv.cvtColor(self.testImg, cv.COLOR_HSV2BGR)
+
     def onClick(self, event, x, y, flags, param):
         
         if event == cv.EVENT_LBUTTONDOWN:
             self.lastClick = (x, y)
-            ret, frame = self.capture.read()
-            rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+            print("click")
+            _, frame = self.capture.read()
+
+            # frame[y, x] = [255, 0, 0]
+            # rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
             hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+            # get pixels in a 3 x 3 grid around the click
+           
+            
             pixel = hsv[y, x]
             lH = min(max(0, pixel[0] - self.hRange), 179)
             lS = min(max(0, pixel[1] - self.sRange), 255)
@@ -72,80 +117,67 @@ class ColorDetector():
             uH = min(max(0, pixel[0] + self.hRange), 179)
             uS = min(max(0, pixel[1] + self.sRange), 255)
             uV = min(max(0, pixel[2] + self.vRange), 255)
+            # set trackbars to the values of the clicked pixel
+            print("tracks")
+            cv.setTrackbarPos("-H", "HSV Range", lH)
+            cv.setTrackbarPos("+H", "HSV Range", uH)
+            cv.setTrackbarPos("-S", "HSV Range", lS)
+            cv.setTrackbarPos("+S", "HSV Range", lS)
+            cv.setTrackbarPos("-V", "HSV Range", lV)
+            cv.setTrackbarPos("+V", "HSV Range", lV)
             self.colorLower = np.array([lH, lS, lV])
             self.colorUpper = np.array([uH, uS, uV])
-
-            # convert pixel to rgb
-            
-            print(f"{frame[y, x][2]}, {frame[y, x][1]}, {frame[y, x][0]}")
-        # if event == cv.EVENT_LBUTTONUP:
-        #     ret, frame = self.capture.read()
-        #     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        #     self.end = (x, y)
-            
-        #     # get all the pixels in the rectangle defined by the start and end points
-        #     # and convert them to a numpy array
-        #     pixels = np.array(list(map(lambda x: hsv[x[1], x[0]], self.getRectangle(self.start, self.end))))
-
-        #     # find the min and max values of the pixels
-        #     minVal = np.min(pixels, axis=0)
-        #     maxVal = np.max(pixels, axis=0)
-
-        #     # find the average of the pixels
-        #     avg = np.average(pixels, axis=0)
-            
-        #     self.colorLower = np.array([avg[0] - self.threshold, avg[1], avg[2]], np.uint8)
-        #     self.colorUpper = np.array([avg[0] + self.threshold, avg[1], avg[2]], np.uint8)
-            
-        #     self.selectedColor = avg
-
-        #     print(hsv[y, x])
-        #     print(self.selectedColor, minVal)
-            # 
-
-            # pixel = hsv[y, x]
-            
-            # self.colorLower = np.array([pixel[0] - self.threshold, pixel[1], pixel[2]], np.uint8)
-            # self.colorUpper = np.array([pixel[0] + self.threshold, pixel[1], pixel[2]], np.uint8)
+            print("done")
 
     def getRectangle(self, start, end):
+        # get the rectangle coordinates from the start and end points
         # get the top left and bottom right points
         x1 = min(start[0], end[0])
         x2 = max(start[0], end[0])
         y1 = min(start[1], end[1])
         y2 = max(start[1], end[1])
-        # return the points
-        return [(x, y) for x in range(x1, x2) for y in range(y1, y2)]
 
-    def onHTrackbarChange(self, val):
-        self.hRange = val
-    def onSTrackbarChange(self, val):
-        self.sRange = val
-    def onVTrackbarChange(self, val):
-        self.vRange = val
+        # get the pixels in the rectangle
+        points = []
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                points.append((y, x))
+        return np.array(points)
+
+    def onLHTrackbarChange(self, val):
+        self.colorLower[0] = val
+    def onUHTrackbarChange(self, val):
+        self.colorUpper[0] = val
+    def onLSTrackbarChange(self, val):
+        self.colorLower[1] = val
+    def onUSTrackbarChange(self, val):
+        self.colorUpper[1] = val
+    def onLVTrackbarChange(self, val):
+        self.colorLower[2] = val
+    def onUVTrackbarChange(self, val):
+        self.colorUpper[2] = val
    
     def run(self):
-
-
-        self.capture = cv.VideoCapture(0)
-        if not self.capture.isOpened():
-            exit(0)
-
         cv.startWindowThread()
         cv.namedWindow('window')
         cv.setMouseCallback("window", self.onClick)
         cv.namedWindow('detector')
 
         cv.namedWindow('HSV Range')
-        cv.createTrackbar("H", "HSV Range", self.hRange, 179, self.onHTrackbarChange)
-        cv.createTrackbar("S", "HSV Range", self.sRange, 255, self.onSTrackbarChange)
-        cv.createTrackbar("V", "HSV Range", self.vRange, 255, self.onVTrackbarChange)
-        
+        self.updateTestImage()
+        cv.createTrackbar(f"-H", "HSV Range", self.hRange, 179, self.onLHTrackbarChange)
+        cv.createTrackbar(f"+H", "HSV Range", self.hRange, 179, self.onUHTrackbarChange)
+        cv.createTrackbar(f"-S", "HSV Range", self.sRange, 255, self.onLSTrackbarChange)
+        cv.createTrackbar(f"+S", "HSV Range", self.sRange, 255, self.onUSTrackbarChange)
+        cv.createTrackbar(f"-V", "HSV Range", self.vRange, 255, self.onLVTrackbarChange)
+        cv.createTrackbar(f"+V", "HSV Range", self.vRange, 255, self.onUVTrackbarChange)
+
         i = 0
         while self.capture.isOpened():
             ret, frame = self.capture.read()
             if frame is None:
                 break
+            self.updateTestImage()
             # mirror the frame
             frame = cv.flip(frame, 1)
             hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
@@ -153,15 +185,10 @@ class ColorDetector():
 
             if self.lastClick is not None:
                 cv.circle(frame, self.lastClick, 5, (0, 0, 255), -1)
-            
-            # cv.line(fgMask, (10, 2), (100, 20), (255, 255, 255), 3)
-            # display text on the frame
-            # if self.selectedColor:
-            #     cv.putText(frame, f'Selected Color: {self.selectedColor[0]} {self.selectedColor[1]} {self.selectedColor[2]}', (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-            # cv.putText(frame, "Select Lower Color", (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-            
-            # cv.putText(frame, f'Threshold value is set to {self.threshold}', (10, 30), cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
+            if self.current_direction is not None:
+                cv.putText(frame, self.current_direction, (10, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+            
             # draw a red line from the upper-left corner to the lower-right corner
             cv.line(mask, (0, 0), (self.width,self.height), (255, 0, 0), 3)
             # draw a red line from the upper-right corner to the lower-left corner
@@ -170,7 +197,12 @@ class ColorDetector():
             #cv.imshow('Frame', frame)
             cv.imshow('window', frame)
             
-            cv.imshow('detector', mask)
+            if self.showNormal:
+                cv.imshow('detector', frame)
+            else:
+                cv.imshow('detector', mask)
+
+            cv.imshow('HSV Range', self.testImg)
 
             if i % 15 == 0:
                 fgMaskMatrix = np.array(mask)
@@ -183,8 +215,9 @@ class ColorDetector():
                 # #send that key to pygame
                 direction_scores = {sum(topDiff): "up", sum(rightDiff): "right", sum(bottomDiff): "down", sum(leftDiff): "left"}
                 max_direction = direction_scores.get(max(direction_scores))
-                print(max_direction)
-
+                self.current_direction = max_direction
+                # print(max_direction)
+                pyautogui.press(max_direction)
             i = i + 1
             keyboard = cv.waitKey(1)
             if keyboard > 0:
@@ -194,82 +227,21 @@ class ColorDetector():
                 # if the - is pressed
                 elif keyboard == ord('-'):
                     self.threshold -= 1
+                elif keyboard == ord('r'):
+                    self.colorLower = self.defaultLower
+                    self.colorUpper = self.defaultUpper
+                elif keyboard == ord('m'):
+                    self.showNormal = not self.showNormal
+                # space is pressed
+                elif keyboard == ord(' '):
+                    pyautogui.press("space")
                 # if the 'q' key is pressed
                 elif keyboard == ord('q'):
                     self.capture.release()
                     break
-
-
-
-# while capture.isOpened():
     
     
 colorDetector = ColorDetector()
 colorDetector.run()
-
-
-#     # set the frame value to 255 if red is greater than threshold
-#     frame[:,:,2] = np.where(frame[:,:,0] > bThreshold, 255, 0)
-#     frame[:,:,1] = np.where(frame[:,:,0] > bThreshold, 255, 0)
-#     frame[:,:,0] = np.where(frame[:,:,0] > bThreshold, 255, 0)
-# print(frame[(0,0)])
-
-# convert any red above the threshold to white, everything else to black
-# frame[:,:,0] = np.where(frame[:,:,0] > threshold, 255, 0)
-# frame[:,:,1] = np.where(frame[:,:,1] > threshold, 255, 0)
-# frame[:,:,2] = np.where(frame[:,:,2] > threshold, 255, 0)
-
-#     # grayscale the frame
-#     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
-#     # apply background subtraction
-
-#     fgMask = gray#backSub.apply(gray)
-    
-#     # Use this to check if the triangles are correct 
-#     # set all values in top to 255
-#     # split too to rows cols array
-#     # fgMask[(np.array(bot)[:,0], np.array(bot)[:, 1])] = 255
-#     # fgMask[upX, upY] = 255
-    
-#     # cv.rectangle(fgMask, (10, 2), (100,20), (255,255,255), -1)
-#     # cv.putText(fgMask, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
-#     #            cv.FONT_HERSHEY_SIMPLEX, 0.5 , (0,0,0))
-
-#     # draw a red line from the upper-left corner to the lower-right corner
-#     cv.line(fgMask, (0, 0), (width,height), (255, 0, 0), 3)
-#     # draw a red line from the upper-right corner to the lower-left corner
-#     cv.line(fgMask, (width, 0), (0,height), (255, 0, 0), 3)
-#     # cv.line(fgMask, (10, 2), (100, 20), (255, 255, 255), 3)
-
-    
-    
-    
-    # fgMaskMatrix = np.array(fgMask)
-    # #calculate which quadrants have most difference
-    # topDiff = [fgMaskMatrix[j] for j in top]
-    # rightDiff = [fgMaskMatrix[j] for j in right]
-    # bottomDiff = [fgMaskMatrix[j] for j in bot]
-    # leftDiff = [fgMaskMatrix[j] for j in left]
-    
-    # # #send that key to pygame
-    # direction_scores = {sum(topDiff): "up", sum(rightDiff): "right", sum(bottomDiff): "down", sum(leftDiff): "left"}
-    # max_direction = direction_scores.get(max(direction_scores))
-    # print(max_direction)
-#     # pyautogui.press(max_direction)
-#     #left and right aren't working, check out their sets or something
-#     # if i % 10 == 0:
-#     #     print(max_direction)
-#     #     print("top: ", sum(topDiff))
-#     #     print("right: ", sum(rightDiff))
-#     #     print("bottom: ", sum(bottomDiff))
-#     #     print("left: ", sum(leftDiff))
-# #         #print(upperTriangleIndices)
-# #         #print(fgMaskMatrix.shape)
-# # #         print(len(fgMask))
-# # #         print(len(fgMask[0]))
-#     #print(fgMask)
-#     i += 1
-    
 
 cv.destroyAllWindows()
